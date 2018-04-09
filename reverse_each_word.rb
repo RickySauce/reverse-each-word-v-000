60d76c4eaa9d00bc08de0654e87e38b4db2c2269
Eric DeSimone <eric.desimone@cortland.edu>
2018-04-09 18:07:19 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,10 @@ sentence = "Hello there, and how are you?"
 
 def reverse_each_word(sentence)
   sentence.split (" ")
-  sentence.reverse!
+  new_sentence = Array.new
+  sentence.each do |word|
+    word.reverse!
+    new_sentence << word
+  end
+  return new_sentence
 end
